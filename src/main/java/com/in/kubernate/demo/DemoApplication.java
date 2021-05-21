befe60f0f44d0c4388e0853b181664ccf33c78e2
ing-berrocal <65179894+ing-berrocal@users.noreply.github.com>
2021-05-21 14:01:54 -0500

    init commit

---
+++ src/main/java/com/in/kubernate/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.in.kubernate.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
